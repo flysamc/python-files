04166e1a1806fca691780b60f2728a9ffa81a744
sam <flysamc@gmail.com>
2016-10-01 13:46:44 +0000

    additin

---
+++ python-datascience/hello.py
@@ -0,0 +1,10 @@
+list1 = ['a','b','c',1,2,3,]
+print(list1)
+#index is start with [0] and from end it starts with one.
+#slicing is done by [3:5] printing only 3,4 not 5
+print(list1[3:5])
+#add more values to a list
+print(list1 + ["ram " , 45.6])
+#delete from a list
+del(list1[4])
+print(list1)
